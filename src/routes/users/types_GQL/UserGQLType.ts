e237be5f7091fc89616979c067d9ca85d6d05369
Yuliya_Shulgan <juliette7373@gmail.com>
2023-01-30 17:17:28 +0100

    feat: complete GQL query

--- src/routes/users/types_GQL/UserGQLType.ts
+++ src/routes/users/types_GQL/UserGQLType.ts
@@ -1,6 +1,11 @@
+import { FastifyInstance } from "fastify";
 import { GraphQLList, GraphQLObjectType, GraphQLString, GraphQLID } from "graphql";
+import { UserEntity } from "../../../utils/DB/entities/DBUsers";
+import { MemberGQLType } from "../../member-types/types_GQL/MemberGQLType";
+import { PostGQLType } from "../../posts/types_GQL/PostGQLType";
+import { ProfileGQLType } from "../../profiles/types_GQL/ProfileGQLType";
 
-export const UserGQLType = new GraphQLObjectType({
+export const UserGQLType: GraphQLObjectType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
     id: { type: GraphQLID },
@@ -8,5 +13,29 @@ export const UserGQLType = new GraphQLObjectType({
     lastName: { type: GraphQLString },
     email: { type: GraphQLString },
     subscribedToUserIds: { type: new GraphQLList(GraphQLString) },
+    profile: {
+      type: ProfileGQLType,
+      resolve: async (user: UserEntity, _, fastify: FastifyInstance) => await fastify.db.profiles.findOne({ key: 'userId', equals: user.id })
+    },
+    posts: {
+      type: new GraphQLList(PostGQLType),
+      resolve: async (user: UserEntity, _, fastify: FastifyInstance) => await fastify.db.posts.findMany({ key: 'userId', equals: user.id })
+    },
+    memberType: {
+      type: MemberGQLType,
+      resolve: async (user: UserEntity, _, fastify: FastifyInstance) => {
+        const userProfile = await fastify.db.profiles.findOne({ key: 'userId', equals: user.id });
+        if (!userProfile) return null;
+        return fastify.db.memberTypes.findOne({ key: 'id', equals: userProfile.memberTypeId });
+      },
+    },
+    userSubscribedTo: {
+      type: new GraphQLList(UserGQLType),
+      resolve: async (user: UserEntity, _, fastify: FastifyInstance) => user.subscribedToUserIds,
+    },
+    subscribedToUser: {
+      type: new GraphQLList(UserGQLType),
+      resolve: async (user: UserEntity, _, fastify: FastifyInstance) => await fastify.db.users.findMany({ key: 'subscribedToUserIds', inArray: user.id }),
+    },
   }),
 })
